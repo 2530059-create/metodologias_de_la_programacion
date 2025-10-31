91b58c5478a3eea9e4cabb0cfdd66336ced9cdaf
juliancarmona <2530059@upv.edu.mx>
2025-10-31 10:31:42 -0600

    feat: understandig_numbers

---
+++ src/understanding_numbers.py
@@ -0,0 +1,67 @@
+#numbers
+
+"""
+  inters - py 
+
+  son numeros que no tienen parte decimal
+  - infinity ,..., -3, -2, -1, 0, 1, 2, 3,... infinity
+
+  Ejemplo: 
+       age=23
+
+  los enteros los podemos sumar, restar, multiplicar y dividir entre si
+
+  potecias (**2, **3, etc)
+ modulo (duvidiendo%divisor)
+  modulo (%)
+
+"""
+
+number_1 =39
+number_2 = 13
+
+sum = number_1 + number_2
+difference = number_1 - number_2
+product = number_1 * number_2
+quotient = number_1 / number_2
+modulo= number_1 % number_2
+power = number_1 ** 2
+
+print ("suma:", sum)
+print ("difference:", difference)
+print ("multiplicacion:", product)
+print ("division:", quotient)
+print ("modulo:", modulo)
+print ("power:", power)
+
+print ("la suma es del tipo:" , type (sum))
+print ("la resta es del tipo:" , type (difference))
+print ("la multiplicacion es del tipo:" , type (product))
+print ("la division es del tipo:" , type (quotient))
+print ("el modulo es del tipo:" , type (modulo))
+print ("la potencia es del tipo:" , type (power))
+
+"""
+el type es para conocer el tipo de dato que estamos utilizando en una variable, entero o real "
+"""
+
+# floats
+
+"""
+floats-reales
+
+son numeros con puntos decimales 
+
+van desde -infty infty
+
+Ejemplos: 
+
+  #Tipado dinamico 
+  age=25.5
+  los podemos restar, multiplicar y divir 
+
+"""
+
+print(0.1+0.1)
+print(0.2-0.2)
+print(0.2*0.2)
