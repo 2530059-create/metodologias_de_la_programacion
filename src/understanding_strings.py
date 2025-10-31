ce634bcb0d26e96acb2e217ed3481a000dbd3b7c
juliancarmona <2530059@upv.edu.mx>
2025-10-31 09:46:30 -0600

    feat: modified the document: first name

--- src/understanding_strings.py
+++ src/understanding_strings.py
@@ -46,4 +46,54 @@ last_name="CARMONA"
 full_name= first_name + " " + last_name
 print(full_name)
 
-print("Hola, " + full_name.title() + "!")
+print ("Hola, " + full_name.title() + "!")
+
+
+# syntax error en strings 
+message="una fortaleza de python es su comunidad"
+print (message)
+
+
+message_two= "una fortaleza de 'python' es su comunidad"
+print(message_two)
+"""
+un error de sintaxis ocurre cuando python no puede interpretar nuestro codigo
+te aviisa en la terminal con un mensaje de error y la ubicacion del error en el codigo.
+
+apararece interpretado de esta manera:
+
+
+    File "c:/Users/julia/projects/metodologias_de_la_programacion/src/understanding_strings.py", line 42
+        message_two= "una fortaleza de 'python' es su comunidad
+                                                                    ^
+
+
+"""
+
+# concatenacion con f-strings
+famous_person="albert einstein"
+quote="python is love"
+
+# concatenacion convencional
+message= famous_person + "una vex dijo " + quote
+print(message)
+
+# concatenacion con f-strings
+
+message_f_strings= f"{famous_person}  una vez dijo  {quote}"
+print(message_f_strings)
+
+
+# ACTIVIDAD
+"""
+1) elije un pesonaje famoso e igualao a una variable del tipo strings
+2)elije una frase famosa que haya dicho e igualao a una variable del tipo string
+3)usa f-strings para crear un mensaje que combine el nombre del personaje y la cita
+4)imprime el mensaje
+"""
+
+famous_person="Socrates"
+quote="solo se que no se nada"       
+message_f_strings= f"{famous_person}  una vez dijo  {quote}"
+print(message_f_strings)
+
